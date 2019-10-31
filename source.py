88654a89baf1ff61c812071a0718cc6bb0cadef1
GorbatovDS <50554014+GorbatovDS@users.noreply.github.com>
2019-10-31 17:32:25 +0300

    Update source.py

--- source.py
+++ source.py
@@ -38,28 +38,6 @@ def Presentation_Detect(web_addr, input_num, pr_ip):
     return_to_address = web_addr + '/api/?Function=Cut&Input=0' # api адресс для переключения обратно в эфир потока на превью
     client = rtsp.Client(rtsp_server_uri=pr_ip) # начало работы с RTSP потоком презентации для сравнения кадров и отслеживания переключения
     image = client.read()
-    while 1:
-        t0 = time.time() # технические выкладки 
-        image1 = client.read() # технические выкладки 
-        print(compare(image, image1)) # технические выкладки 
-        t1 = time.time() # технические выкладки 
-        print(t1-t0) # технические выкладки 
-        k = compare(image, image) # коэффициент "изменения" кадра (______надо доработать______)
-        if compare(image, image1) > k:
-            image = client.read()
-            print(rq.get(presentation_address)) # переключение на презентацию и вывод ответа
-            time.sleep(5)
-            print(rq.get(return_to_address)) # переключение на превью и вывод ответа
-            '''
-            1xx Informational – the request was received, continuing process
-            ___2xx Successful – the request was successfully received, understood and accepted
-            3xx Redirection – further action needs to be taken in order to complete the request
-            ___4xx Client Error – the request contains bad syntax or cannot be fulfilled
-            5xx Server Error – the server failed to fulfill an apparently valid request
-            '''
-
-
-def Change(web_address):
     random.seed()
     # Настройки vMix таковы:
     # 1: презентация
@@ -69,31 +47,45 @@ def Change(web_address):
     # 5: rtsp://172.18.200.54:554/Streaming/Channels/1
     # 6: rtsp://172.18.200.55:554/Streaming/Channels/1
     # 7: rtsp://172.18.200.56:554/Streaming/Channels/1
-    number = ['2', '7', '4', '3', '6', '5']
-    # В списке - номера потоков камер в vMix.
+    # В списке numbers - номера потоков камер в vMix.
     # Переключение происходит именно в таком порядке. Закольцованно.
+    number = ['2', '7', '4', '3', '6', '5']
     i = 0
-    print(number[i])
-    while not i == -1:
-        time_crowd = random.randint(5, 15)
-        time_speaker = random.randint(15, 35)
-        change_to = web_address + '/api/?Function=Cut&Input=' + number[i]
-        print('Переключение камер ', rq.get(url=change_to))
-        if (i % 2) == 0:
-            time.sleep(time_crowd)
+    while 1:
+        time_crowd = random.randint(5, 15) # время кадра на экране для аудитории
+        time_speaker = random.randint(15, 35) # время кадра на экране для докладчика
+        image1 = client.read()
+        print(compare(image, image1))
+        k = compare(image, image) # коэффициент "изменения" кадра (______надо доработать______)
+        if compare(image, image1) > k:
+            image = client.read()
+            print(rq.get(presentation_address)) # переключение на презентацию и вывод ответа
+            time.sleep(5)
+            print(rq.get(return_to_address)) # переключение на превью и вывод ответа
         else:
-            time.sleep(time_speaker)
-        i = i + 1
-        if i == 6:
-            i = 0
+            change_to = web_addr + '/api/?Function=Cut&Input=' + number[i] # 
+            print('Переключение камер ', rq.get(url=change_to))
+            if (i % 2) == 0:
+                time.sleep(time_crowd)
+            else:
+                time.sleep(time_speaker)
+            i = i + 1
+            if i == 6:
+                i = 0
+            '''
+            1xx Informational – the request was received, continuing process
+            ___2xx Successful – the request was successfully received, understood and accepted
+            3xx Redirection – further action needs to be taken in order to complete the request
+            ___4xx Client Error – the request contains bad syntax or cannot be fulfilled
+            5xx Server Error – the server failed to fulfill an apparently valid request
+            '''
 
 
 def main():
     a, b, c = GetConfig()
     # print(a,b,c)
     # Presentation_Detect(a, b, c)
-    # threading.Thread(target=Presentation_Detect, args=[a, b, c], daemon=True).start()
-    threading.Thread(target=Change, args=[a], daemon=True).start()
+    threading.Thread(target=Presentation_Detect, args=[a, b, c], daemon=True).start()
     input('Press <Enter> to exit.\n')
     cv2.destroyAllWindows()
 
